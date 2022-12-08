33cf2cb9573dbef9d060dbcaaeb0ca4442964815
Trung <truongcongtrung2709@gmail.com>
2022-12-09 00:35:30 +0700

    + updated TaoLichChieu

--- src/components/CinemaModal/CinemaModal.jsx
+++ src/components/CinemaModal/CinemaModal.jsx
@@ -5,19 +5,21 @@ import "./cinemaModal.scss";
 import DateTimePicker from "react-datetime-picker";
 import { useForm } from "react-hook-form";
 import { useMemo } from "react";
+import moment from "moment";
 
 const CinemaModal = ({ calendaMovie, show, handleClose }) => {
   const [cinema, setCinema] = useState([]);
   const [cinemaBranches, setCinemaBranches] = useState([]);
   const [cinemaSelected, setCinemaSelected] = useState("");
   const [cinemaBranchSelected, setCinemaBranchSelected] = useState("");
+
   const [dateTimeValue, setDateTimeValue] = useState(new Date());
-  const [danhSachRap, setDanhSachRap] = useState([])
-  const [numCinemaSelected, setNumCinemaSelected] = useState("");
-  
+
+  const [cumRap, setCumRap] = useState([]);
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
     defaultValues: useMemo(() => {
@@ -47,27 +49,22 @@ const CinemaModal = ({ calendaMovie, show, handleClose }) => {
     }
   }, [cinemaSelected]);
 
-
   const handleChangeCinema = (e) => {
     setCinemaSelected(e.target.value);
   };
 
   const handleChangeCinemaBranch = (e) => {
     setCinemaBranchSelected(e.target.value);
-     if(cinemaBranchSelected !== ""){
-
-      setDanhSachRap(cinemaBranches.find((item) => item.maCumRap === cinemaBranchSelected).danhSachRap)
-    
-    }else{
-      alert("Nhập lại cụm rạp")
+    if (cinemaBranchSelected !== "") {
+      setCumRap(
+        cinemaBranches.find((item) => item.maCumRap === cinemaBranchSelected)
+      );
+    } else {
+      alert("Nhập lại cụm rạp");
       return;
     }
-    
   };
-  const handleChangeTenRap = (e) =>{
-    setNumCinemaSelected(e.target.value);
 
-  }
   useEffect(() => {
     (async () => {
       try {
@@ -79,17 +76,23 @@ const CinemaModal = ({ calendaMovie, show, handleClose }) => {
     })();
   }, []);
 
-
   const onSubmit = async (values) => {
     try {
+      setDateTimeValue(moment().format("DD/MM/yyyy hh:mm:ss"));
       const newValues = {
         maPhim: calendaMovie.maPhim,
         ngayChieuGioChieu: dateTimeValue,
-        maRap: numCinemaSelected,
+        maRap: cumRap.maCumRap,
         giaVe: values.giaVe,
-      }
-      console.log(calendaMovie.maPhim,dateTimeValue,numCinemaSelected,values.giaVe,);
-      await moviesManagementAPI.taoLichChieu(newValues)
+      };
+      console.log(
+        calendaMovie.maPhim,
+        dateTimeValue,
+        cumRap.maCumRap,
+        values.giaVe
+      );
+      await moviesManagementAPI.taoLichChieu(newValues);
+      handleClose();
     } catch (error) {
       console.log(error);
     }
@@ -102,8 +105,8 @@ const CinemaModal = ({ calendaMovie, show, handleClose }) => {
       <Modal.Body>
         <div className="row">
           <div className="poster col-6">
-            <img src={calendaMovie.hinhAnh} alt="" width={400} />
-            <h5>{calendaMovie.tenPhim}</h5>
+            <img src={calendaMovie.hinhAnh} alt="" width="100%" />
+            <h5 className="w-100">{calendaMovie.tenPhim}</h5>
           </div>
           <div className="cinema__input-group col-6">
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -135,40 +138,27 @@ const CinemaModal = ({ calendaMovie, show, handleClose }) => {
                     </option>
                   ))}
                 </select>
-                <label htmlFor="">Số Rạp:</label>
-                <select
-                  name=""
-                  id=""
-                  className="w-100"
-                  onChange={handleChangeTenRap}
-                >
-                  {danhSachRap.map((item) => (
-                    <option key={item.maRap} value={item.maRap}>
-                      {item.tenRap}
-                    </option>
-                  ))}
-                </select>
                 <label className="w-100" htmlFor="">
                   Ngày Chiếu Giờ Chiếu:
                 </label>
                 <DateTimePicker
                   onChange={setDateTimeValue}
-                  
                   value={dateTimeValue}
                   format="dd-MM-yyyy hh:mm:ss"
                   locale="vi-VI"
                   minDate={new Date()}
-                    
-                  
                 />
 
                 <label className="w-100" htmlFor="">
                   Giá vé:
                 </label>
-                <input type="number" className="w-90" {...register("giaVe",{
-                  required: "Không được để trống",
-                
-                })} />
+                <input
+                  type="number"
+                  className="w-90"
+                  {...register("giaVe", {
+                    required: "Không được để trống",
+                  })}
+                />
                 <span className="mx-2">VND</span>
               </div>
               {errors.giaVe && (
